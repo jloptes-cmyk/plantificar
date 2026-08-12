50ef402a1ee70481b2081fe4f04e85a0b160a43e
jloptes-cmyk <jloptes@gmail.com>
2026-08-12 16:23:47 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE="plantificador-v5-plant-pests";
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./manifest.webmanifest","./favicon.svg"]))));
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener("activate",event=>event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||(event.request.mode==="navigate"?caches.match("./"):undefined))))});
